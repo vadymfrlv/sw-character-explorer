cc676d5d99fd57d18888d059cb4f6a306f3e9c7d
Vadym Frolov <vdfroloff@gmail.com>
2024-01-04 14:21:57 +0200

    feat: header logo to reset application
    Click on logo will reset all and return user back to 1 page.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useCharacters } from 'hooks/useCharacters';
 
 import { HeaderStyled, Logo } from './Header.styled';
 
 export const Header = () => {
+  const { setCurrentPage } = useCharacters();
+
   return (
     <HeaderStyled>
-      <Link to="/">
+      <Link to="/" onClick={() => setCurrentPage(1)}>
         <Logo />
       </Link>
     </HeaderStyled>
