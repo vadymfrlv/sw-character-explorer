aaf7b3f01057528bc83b6f7db7316e64662d61e5
Vadym Frolov <vdfroloff@gmail.com>
2024-01-04 12:45:24 +0200

    feat: added QueryClient and QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
 import theme from 'styles/theme.ts';
@@ -10,12 +11,16 @@ import 'styles/fonts.css';
 import 'styles/global.css';
 import 'styles/reset.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
