b102b08d2261a2eeb4e50c8fe8441ee32afc603f
Vadym Frolov <vdfroloff@gmail.com>
2024-01-04 14:01:15 +0200

    feat: updated character info page
    Added VisuallyHidden and BackButton/

--- src/pages/CharacterInfoPage.tsx
+++ src/pages/CharacterInfoPage.tsx
@@ -1,5 +1,13 @@
-const CharacterInfoPage = () => {
-  return <div>CharacterInfoPage</div>;
+import { BackButton } from 'components/BackButton/BackButton';
+import { VisuallyHidden } from 'components/Common/VisuallyHidden';
+
+const CharacterDetailsPage = () => {
+  return (
+    <>
+      <VisuallyHidden as="h1">Character details</VisuallyHidden>
+      <BackButton />
+    </>
+  );
 };
 
-export default CharacterInfoPage;
+export default CharacterDetailsPage;
