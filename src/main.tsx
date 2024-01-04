383727865d2e2929e31c5e90ab9657aec68b3f8c
Vadym Frolov <vdfroloff@gmail.com>
2024-01-04 08:33:10 +0200

    style: global styles
    Creating a global style, reset parameters, font connection params. Connecting styles and fonts to the app.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
+import 'styles/fonts.css';
+import 'styles/global.css';
+import 'styles/reset.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
